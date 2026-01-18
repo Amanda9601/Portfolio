8986acac79f9ca6c4b19c22d2fd5d612ac45e07c
Amanda9601 <11021228hilwsfb@gmail.com>
2026-01-18 17:49:54 +0800

    modify name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,8 +27,7 @@ const Navbar = () => {
         >
            <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
             <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Amanda Goh &nbsp; 
-            <span className='sm:block hidden'>| Python </span>
+            Amanda Goh Shi Zhen
           </p>
         </Link>
 
