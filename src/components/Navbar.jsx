3646ad207406d9c8c1c76dbd2e091fbf9fab2e7f
Amanda9601 <11021228hilwsfb@gmail.com>
2026-01-23 12:51:10 +0800

    Modify name in navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
         >
            <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
             <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Amanda Goh Shi Zhen
+            Amanda Goh
           </p>
         </Link>
 
