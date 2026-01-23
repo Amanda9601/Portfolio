b455adda7a11b7f3b5f1fbb3ad2e097214d5fcb8
Amanda9601 <11021228hilwsfb@gmail.com>
2026-01-23 13:53:55 +0800

    Resize the 3d computer according to the screen size but only works for width need to find way to fix height

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,8 +6,7 @@ from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
-  
+const Computers = ({isMobile}) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   
   
@@ -25,8 +24,8 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={1.5}
-        position={[0, -6, -1.5]}
+        scale={isMobile ? 1 : 1.5}
+        position={isMobile ? [0, -5, -2.2 ] : [0, -6, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -34,6 +33,32 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+
+  const [isMobile, setIsMoblie] = useState(false);
+
+  // Changing the isMobile variable
+  useEffect(() => {
+
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia('(max-width: 500px');
+
+    // Set the initial value of the 'isMobile' state variable
+    setIsMoblie(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMoblie(event.matches);
+    }
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+    
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+  }, [])
+
   return (
     <Canvas
       frameloop='demand'
@@ -47,7 +72,7 @@ const ComputersCanvas = () => {
             maxPolarAngle={Math.PI /2} 
             minPolarAngle={Math.PI /2}
           />
-          <Computers/>
+          <Computers isMobile={isMobile}/>
         </Suspense>
 
         <Preload all/>
